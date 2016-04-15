cb9f7a526f98af49d48f178632d52246b7468c37
Iolane Andrade <andrade.icaa@gmail.com>
2016-04-15 08:42:59 -0300

    Course: asserts completed

--- QualCurso/app/src/main/java/models/Course.java
+++ QualCurso/app/src/main/java/models/Course.java
@@ -5,6 +5,8 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class Course extends Bean implements Parcelable{
@@ -18,6 +20,7 @@ public class Course extends Bean implements Parcelable{
 	}
 	
 	public Course(int id){
+		assert (id >= 0) : "Receive a negative tratment";
 		this.id = id;
 		this.identifier= "course";
 		this.relationship = "courses_institutions";
@@ -28,6 +31,7 @@ public class Course extends Bean implements Parcelable{
 	}
 	
 	public void setId(int id) {
+		assert (id >= 0) : "Receive a negative tratment";
 		this.id = id;
 	}
 	
@@ -36,6 +40,9 @@ public class Course extends Bean implements Parcelable{
 	}
 
 	public void setName(String name) {
+		assert (name != null) : "Receive a null tratment";
+		assert (name != "") : "Receive a empty tratment";
+		assert (name.length() > 1) : "Receive a size tratment";
 		this.name = name;
 	}
 	
@@ -48,23 +55,22 @@ public class Course extends Bean implements Parcelable{
 	}
 	
 	public boolean addInstitution(Institution institution) throws  SQLException {
+		assert (institution != null) : "Receive a null tratment";
 		boolean result = false;
 		GenericBeanDAO gDB = new GenericBeanDAO();
 		result = gDB.addBeanRelationship(this, institution);
 		return result;
 	}
 
-
-	public static Course get(int id) throws 
-			SQLException {
+	public static Course get(int id) throws SQLException {
+		assert (id >= 0) : "Receive a negative tratment";
 		Course result = new Course(id);
 		GenericBeanDAO gDB = new GenericBeanDAO();
 		result = (Course) gDB.selectBean(result);
 		return result;
 	}
 
-	public static ArrayList<Course> getAll()
-			throws  SQLException {
+	public static ArrayList<Course> getAll() throws  SQLException {
 		Course type = new Course();
 		ArrayList<Course> result = new ArrayList<Course>();
 		GenericBeanDAO gDB = new GenericBeanDAO();
@@ -74,22 +80,20 @@ public class Course extends Bean implements Parcelable{
 		return result;
 	}
 
-	public static int count() throws  SQLException {
+	public static int count() throws SQLException {
 		Course type = new Course();
 		GenericBeanDAO gDB = new GenericBeanDAO();
 		return gDB.countBean(type);
 	}
 
-	public static Course first() throws 
-			SQLException {
+	public static Course first() throws SQLException {
 		Course result = new Course();
 		GenericBeanDAO gDB = new GenericBeanDAO();
 		result = (Course) gDB.firstOrLastBean(result, false);
 		return result;
 	}
 
-	public static Course last() throws 
-			SQLException {
+	public static Course last() throws SQLException {
 		Course result = new Course();
 		GenericBeanDAO gDB = new GenericBeanDAO();
 		result = (Course) gDB.firstOrLastBean(result, true);
@@ -106,8 +110,11 @@ public class Course extends Bean implements Parcelable{
 		return institutions;
 	}
 	
-	public ArrayList<Institution> getInstitutions(int year) throws 
-			SQLException {
+	public ArrayList<Institution> getInstitutions(int year) throws SQLException {
+		Calendar data = Calendar.getInstance();
+		int yearData = data.get(Calendar.YEAR);
+		assert (year > 0) : "Receive a negative tratment";
+		assert (year <= yearData) : "Receive a max year tratment";
 		ArrayList<Institution> institutions = new ArrayList<Institution>();
 		GenericBeanDAO gDB = new GenericBeanDAO();
 		for (Bean b : gDB.selectBeanRelationship(this, "institution",year,"acronym")) {
@@ -116,8 +123,11 @@ public class Course extends Bean implements Parcelable{
 		return institutions;
 }
 
-	public static ArrayList<Course> getWhere(String field, String value,
-			boolean like) throws  SQLException {
+	public static ArrayList<Course> getWhere(String field, String value, boolean like) throws  SQLException {
+		assert (field != null) : "Receive a null tratment";
+		assert (field != "") : "Receive a empty tratment";
+		assert (value != null) : "Receive a null tratment";
+		assert (value != "") : "Receive a null tratment";
 		Course type = new Course();
 		ArrayList<Course> result = new ArrayList<Course>();
 		GenericBeanDAO gDB = new GenericBeanDAO();
@@ -128,6 +138,7 @@ public class Course extends Bean implements Parcelable{
 	}
 	
 	public static ArrayList<Course> getCoursesByEvaluationFilter(Search search) throws  SQLException {
+		assert (search != null) : "Receive a null tratment";
 		ArrayList<Course> result = new ArrayList<Course>();
 		String sql = "SELECT c.* FROM course AS c, evaluation AS e, articles AS a, books AS b "+
 					" WHERE year="+Integer.toString(search.getYear())+
@@ -153,6 +164,8 @@ public class Course extends Bean implements Parcelable{
 	}
 
 	public static ArrayList<Institution> getInstitutionsByEvaluationFilter(int id_course, Search search) throws  SQLException {
+		assert (id_course > 0) : "Receive a negative trament";
+		assert (search != null) : "Receive a null tratment";
 		ArrayList<Institution> result = new ArrayList<Institution>();
 		String sql = "SELECT i.* FROM institution AS i, evaluation AS e, articles AS a, books AS b "+
 					" WHERE e.id_course="+Integer.toString(id_course)+
@@ -188,6 +201,8 @@ public class Course extends Bean implements Parcelable{
 	
 	@Override
 	public String get(String field) {
+		assert (field != null) : "Receive a null tratment";
+		assert (field != "") : "Receive a empty tratment";
 		if(field.equals("_id")){
 			return Integer.toString(this.getId());
 		}else if(field.equals("name")){
@@ -199,14 +214,18 @@ public class Course extends Bean implements Parcelable{
 
 	@Override
 	public void set(String field, String data) {
+		assert (field != null) : "Receive a null tratment";
+		assert (field != "") : "Receive a empty tratment";
+		assert (data != null) : "Receive a null tratment";
+		assert (data != "") : "Receive a empty tratment";
+
 		if(field.equals("_id")){
 			this.setId(Integer.parseInt(data));
 		}else if(field.equals("name")){
 			this.setName(data);
 		}else {
-		
+			/* Nothing to do! */
 		}
-		
 	}
 
 	@Override
@@ -223,6 +242,7 @@ public class Course extends Bean implements Parcelable{
 	}
 	
 	private Course(Parcel in){
+		assert (in != null) : "Receive a null tratment";
 		this.id = in.readInt();
 		this.name = in.readString();
 		this.identifier = in.readString();
@@ -237,22 +257,25 @@ public class Course extends Bean implements Parcelable{
 
 	@Override
 	public void writeToParcel(Parcel dest, int flags) {
+		assert (dest != null) : "Receive a null tratment";
+		assert (flags > 0) : "Receive a negative tratment";
 		dest.writeInt(this.id);
 		dest.writeString(this.name);
 		dest.writeString(this.identifier);
 		dest.writeString(this.relationship);
-		
 	}
 	
 	public static final Parcelable.Creator<Course> CREATOR = new Parcelable.Creator<Course>() {
 
 		@Override
 		public Course createFromParcel(Parcel source) {
+			assert (source != null) : "Receive a null tratment";
 			return new Course(source);
 		}
 
 		@Override
 		public Course[] newArray(int size) {
+			assert (size != 0) : "Receive a size tratment";
 			// TODO Auto-generated method stub
 			return new Course[size];
 		}

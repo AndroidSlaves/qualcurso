d3e3f0c164d5fe3c19f6c495544b179af0bb877b
Pedro Ivo <andradepedroivo@gmail.com>
2016-06-03 10:03:13 -0300

    JavaDoc ListCompareAdapter

--- QualCurso/app/src/main/java/unb/mdsgpp/qualcurso/ListCompareAdapter.java
+++ QualCurso/app/src/main/java/unb/mdsgpp/qualcurso/ListCompareAdapter.java
@@ -1,3 +1,9 @@
+/************************
+ * Class name: ListCompareAdapter (.java)
+ *
+ * Purpose: Inflates the structure of the list screen of the institutions.
+ ************************/
+
 package unb.mdsgpp.qualcurso;
 
 import java.util.ArrayList;
@@ -18,16 +24,36 @@ import android.widget.ListView;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class ListCompareAdapter extends ArrayAdapter<Institution> implements OnCheckedChangeListener{
-	
+
+	// Defines the structure of the term presented institutions, word formatting.
 	public static int INSTITUTION = R.string.institution;
+	// Indicates the selected position of the item in the CheckBox component list.
 	public static int POSITION = R.id.checkbox;
+	// Calls to call another Fragment identifying call.
 	private Fragment callingFragment = null;
+	// Used to identify which item from the list of institutions was selected by the user.
 	private CheckBoxListCallbacks checkBoxCallBacks;
+	// Component used to list the institutions.
 	private CheckBox cb = null;
+	// List of items within the CheckBox component to the user's selection.
 	private ArrayList<Boolean> checkedItems = new ArrayList<Boolean>();
-	
+
+	/**
+	 * Rescuing Father class attribute values, sets the Fragment type attribute to call fragment
+	 * creates the list and add items to a list to be used in the application.
+	 *
+	 * @param context
+	 * 				Refers to the context that class where instantiated.
+	 * @param resource
+	 *				Value that will be placed on Class Father.
+	 * @param item
+	 * 				Which item of the list will be shown.
+	 * @param callingFragment
+	 * 				Fragment that will be shown.
+     */
 	@SuppressLint("Assert")
-	public ListCompareAdapter(Context context, int resource, List<Institution> item, Fragment callingFragment) {
+	public ListCompareAdapter(Context context, int resource, List<Institution> item,
+							  Fragment callingFragment) {
 		super(context, resource, item);
 		assert (context != null) : "Receive the null context of treatment";
 		assert (resource > 0) : "Treatment to lower value of resource";
@@ -39,7 +65,20 @@ public class ListCompareAdapter extends ArrayAdapter<Institution> implements OnC
 			checkedItems.add(false);
 		}
 	}
-	
+
+	/**
+	 * Works to inflate the user view screen showing the list CheckBox, Selects and transmits the
+	 * data of the selected institution for users.
+	 *
+	 * @param position
+	 * 				Id of item clicked by user.
+	 * @param contextView
+	 * 				The actual view that user sees.
+	 * @param parent
+	 * 				The view pattern.
+     * @return
+	 * 				View selected by user.
+     */
 	@SuppressLint("InflateParams")
 	@Override
 	public View getView(int position, View contextView, ViewGroup parent){
@@ -67,6 +106,14 @@ public class ListCompareAdapter extends ArrayAdapter<Institution> implements OnC
 		return currentView;
 	}
 
+	/**
+	 * Select the item to be clicked, used to return the position of the list button.
+	 *
+	 * @param buttonView
+	 * 				Which button is clicked.
+	 * @param isChecked
+	 * 				Store information about user clicking this checkBox.
+     */
 	@Override
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 		// TODO Auto-generated method stub

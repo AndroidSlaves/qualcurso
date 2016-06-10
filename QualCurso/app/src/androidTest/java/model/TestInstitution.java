3599c5ceed59dc3206ed8f106e4ffee3edef5368
Lucas Brilhante <lbrilhante82@gmail.com>
2016-06-10 18:17:23 -0300

    Continuing test on Institution

--- QualCurso/app/src/androidTest/java/model/TestInstitution.java
+++ QualCurso/app/src/androidTest/java/model/TestInstitution.java
@@ -2,8 +2,14 @@ package model;
 
 import android.test.AndroidTestCase;
 
+import java.util.ArrayList;
+
+import helpers.Indicator;
 import libraries.DataBaseStructures;
+import models.Course;
+import models.Evaluation;
 import models.Institution;
+import models.Search;
 import unb.mdsgpp.qualcurso.QualCurso;
 
 public class TestInstitution extends AndroidTestCase {
@@ -55,4 +61,84 @@ public class TestInstitution extends AndroidTestCase {
         assertEquals("two", Institution.getAll().get(1).getAcronym());
         assertEquals("", Institution.first().get("test"));
     }
+    public void testShouldGetInstitutionOnDataBase() {
+        assertEquals("one", Institution.get(1).getAcronym());
+        assertEquals(Institution.getAll().get(0).getAcronym(), Institution.get(1).getAcronym());
+    }
+
+
+    public void testShouldGetFirstInstitutionOnDataBase() {
+        assertEquals("one", Institution.first().getAcronym());
+        assertEquals(Institution.getAll().get(0).getAcronym(), Institution
+                .first().getAcronym());
+        assertEquals("one", Institution.first().toString());
+    }
+
+
+    public void testShouldGetLastInstitutionOnDataBase() {
+        assertEquals("two", Institution.last().getAcronym());
+        assertEquals(Institution.getAll().get(1).getAcronym(), Institution
+                .last().getAcronym());
+    }
+
+
+    public void testShouldGetInstitutionCoursesOnDataBase() {
+        Institution institution = new Institution();
+        institution.setAcronym("three");
+        institution.save();
+        Course course = new Course();
+        course.setName("Java");
+        course.save();
+        institution.addCourse(course);
+        Course course1 = new Course();
+        course1.setName("Phyton");
+        course1.save();
+        institution.addCourse(course1);
+        assertEquals("Java", Institution.last().getCourses().get(0).getName());
+        assertEquals("Phyton", Institution.last().getCourses().get(1).getName());
+        assertEquals(2, Institution.last().getCourses().size());
+        institution.delete();
+        course.delete();
+        course1.delete();
+    }
+
+    public void testShouldCreateInstitutionCourseOnDataBase() {
+        Institution institution = new Institution();
+        institution.setAcronym("three");
+        institution.save();
+        Course course = new Course();
+        course.setName("Java");
+        course.save();
+        institution.addCourse(course);
+        assertEquals("Java", Institution.last().getCourses().get(0).getName());
+        assertEquals(course.getId(), Institution.last().getCourses().get(0).getId());
+        institution.delete();
+        course.delete();
+    }
+
+
+    public void testShouldGetInstitutionsWithWhereOnDataBase(){
+        Institution institution = new Institution();
+        institution.setAcronym("Institution AA");
+        institution.save();
+
+        Institution institution1 = new Institution();
+        institution1.setAcronym("Institution BB");
+        institution1.save();
+
+        ArrayList<Institution> institutions1 = Institution.getWhere("acronym", "Institution", true);
+
+        ArrayList<Institution> institutions2 = Institution.getWhere("acronym", "AA", true);
+
+        ArrayList<Institution> institutions3 = Institution.getWhere("acronym", "Institution AA", false);
+
+        assertEquals(2, institutions1.size());
+        assertEquals(1, institutions2.size());
+        assertEquals(1, institutions3.size());
+        institution.delete();
+        institution1.delete();
+    }
+
+
+
 }

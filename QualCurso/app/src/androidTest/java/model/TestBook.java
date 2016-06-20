58c3fae87447fe873bd627e00ecadcba7e51c28b
Iolane Andrade <andrade.icaa@gmail.com>
2016-06-20 06:18:39 -0300

    Book: Testes

--- QualCurso/app/src/androidTest/java/model/TestBook.java
+++ QualCurso/app/src/androidTest/java/model/TestBook.java
@@ -31,98 +31,105 @@ public class TestBook extends AndroidTestCase {
     }
 
     public void testSetIntegralTextBook(){
-        Book idBook = new Book();
-        idBook.setId(21);
-        assertEquals(idBook.getId(), 21);
+        Book integralBook = new Book();
+        integralBook.setIntegralText(21);
+        assertEquals(integralBook.getIntegralText(), 21);
     }
 
     public void testSetIntegralTextBookMustFail(){
-        Book idBook = new Book();
-        idBook.setId(-10);
-        assertNotSame(idBook.getId(), 10);
+        Book integralBook = new Book();
+        integralBook.setIntegralText(-10);
+        assertNotSame(integralBook.getIntegralText(), 10);
     }
 
     public void testGetIntegralTextBook(){
-        Book idBook = new Book();
-        idBook.setId(21);
-        assertEquals(21, idBook.getId());
+        Book integralBook = new Book();
+        integralBook.setIntegralText(21);
+        assertEquals(21, integralBook.getIntegralText());
     }
 
     public void testGetIntegralTextBookMustFail(){
-        Book idBook = new Book();
-        idBook.setId(-155);
-        assertNotSame(idBook.getId(), 551);
+        Book integralBook = new Book();
+        integralBook.setIntegralText(-155);
+        assertNotSame(integralBook.getIntegralText(), 551);
     }
 
     public void testSetChaptersBook(){
-        Book idBook = new Book();
-        idBook.setId(25);
-        assertEquals(idBook.getId(), 25);
+        Book chaptersBook = new Book();
+        chaptersBook.setChapters(25);
+        assertEquals(chaptersBook.getChapters(), 25);
     }
 
     public void testSetChaptersBookMustFail(){
-        Book idBook = new Book();
-        idBook.setId(-11);
-        assertNotSame(idBook.getId(), 13);
+        Book chaptersBook = new Book();
+        chaptersBook.setChapters(-11);
+        assertNotSame(chaptersBook.getChapters(), 13);
     }
 
     public void testGetChaptersBook(){
-        Book idBook = new Book();
-        idBook.setId(26);
-        assertEquals(26, idBook.getId());
+        Book chaptersBook = new Book();
+        chaptersBook.setChapters(26);
+        assertEquals(26, chaptersBook.getChapters());
     }
 
     public void testGetChaptersBookMustFail(){
-        Book idBook = new Book();
-        idBook.setId(-175);
-        assertNotSame(idBook.getId(), 571);
+        Book chaptersBook = new Book();
+        chaptersBook.setChapters(-175);
+        assertNotSame(chaptersBook.getChapters(), 571);
     }
 
     public void testSetCollectionsBook(){
-        Book idBook = new Book();
-        idBook.setId(21);
-        assertEquals(idBook.getId(), 21);
+        Book collectionsBook = new Book();
+        collectionsBook.setCollections(21);
+        assertEquals(collectionsBook.getCollections(), 21);
     }
 
     public void testSetCollectionsBookMustFail(){
-        Book idBook = new Book();
-        idBook.setId(-33);
-        assertNotSame(idBook.getId(), 13);
+        Book collectionsBook = new Book();
+        collectionsBook.setCollections(-33);
+        assertNotSame(collectionsBook.getCollections(), 13);
     }
 
     public void testGetCollectionsBook(){
-        Book idBook = new Book();
-        idBook.setId(31);
-        assertEquals(31, idBook.getId());
+        Book collectionsBook = new Book();
+        collectionsBook.setCollections(31);
+        assertEquals(31, collectionsBook.getCollections());
     }
 
     public void testGetCollectionsBookMustFail(){
-        Book idBook = new Book();
-        idBook.setId(-185);
-        assertNotSame(idBook.getId(), 871);
+        Book collectionsBook = new Book();
+        collectionsBook.setCollections(-185);
+        assertNotSame(collectionsBook.getCollections(), 871);
     }
 
     public void testSetEntriesBook(){
-        Book idBook = new Book();
-        idBook.setId(12);
-        assertEquals(idBook.getId(), 12);
+        Book entriesBook = new Book();
+        entriesBook.setEntries(12);
+        assertEquals(entriesBook.getEntries(), 12);
     }
 
     public void testSetEntriesBookMustFail(){
-        Book idBook = new Book();
-        idBook.setId(-42);
-        assertNotSame(idBook.getId(), 28);
+        Book entriesBook = new Book();
+        entriesBook.setEntries(-42);
+        assertNotSame(entriesBook.getEntries(), 28);
     }
 
     public void testGetEntriesBook(){
-        Book idBook = new Book();
-        idBook.setId(42);
-        assertEquals(42, idBook.getId());
+        Book entriesBook = new Book();
+        entriesBook.setEntries(42);
+        assertEquals(42, entriesBook.getEntries());
     }
 
     public void testGetEntriesBookMustFail(){
-        Book idBook = new Book();
-        idBook.setId(-223);
-        assertNotSame(idBook.getId(), 441);
+        Book entriesBook = new Book();
+        entriesBook.setEntries(-223);
+        assertNotSame(entriesBook.getEntries(), 441);
     }
+
+    public void testSaveBook(){
+        Book entriesBook = new Book();
+        boolean result = entriesBook.save();
+        assertTrue(result);
+    }
+
 }

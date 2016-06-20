3186ed9211f34eaccfc5e2a01b8767fa34e9a19c
Pedro Ivo <andradepedroivo@gmail.com>
2016-06-20 05:14:25 -0300

    Adding last tests to TestSearch

--- QualCurso/app/src/androidTest/java/model/TestSearch.java
+++ QualCurso/app/src/androidTest/java/model/TestSearch.java
@@ -61,5 +61,38 @@ public class TestSearch extends AndroidTestCase {
         testAndroidTestCaseSetupProperly();
     }
 
-    
+    public void testShouldGetSearch() {
+        Search lastSearch = Search.last();
+
+        assertEquals(lastSearch.getId(), Search.get(lastSearch.getId()).getId());
+    }
+
+    public void testShouldGetAllSearches() {
+        int searchCount = Search.count();
+
+        assertEquals(searchCount, Search.getAll().size());
+    }
+
+    public void testShouldGetFirstAndLastSearches() {
+        Search firstSearch = Search.first();
+        Search lastSearch = Search.last();
+
+        assertEquals(10, firstSearch.getMaxValue());
+        assertEquals(15, lastSearch.getMaxValue());
+    }
+
+    public void testShouldGetSearchBySomeValue() {
+        Search search = Search.getWhere("max_value", "10", false).get(0);
+        int searchValue = 10;
+
+        assertEquals(searchValue, search.getMaxValue());
+    }
+
+    public void testShouldDeleteSearches() {
+        while(Search.count() > 0)
+            Search.last().delete();
+
+        assertEquals(0, Search.count());
+        testAndroidTestCaseSetupProperly();
+    }
 }

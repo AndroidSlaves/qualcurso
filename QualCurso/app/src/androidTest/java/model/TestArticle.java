e3c5b5b8613846759aacf9436d0b72ac4dd60727
Pedro Ivo <andradepedroivo@gmail.com>
2016-06-20 03:57:16 -0300

    TestArticle database article tests

--- QualCurso/app/src/androidTest/java/model/TestArticle.java
+++ QualCurso/app/src/androidTest/java/model/TestArticle.java
@@ -3,6 +3,8 @@ package model;
 import android.database.SQLException;
 import android.test.AndroidTestCase;
 
+import java.util.ArrayList;
+
 import libraries.DataBaseStructures;
 import models.Article;
 import unb.mdsgpp.qualcurso.QualCurso;
@@ -56,4 +58,49 @@ public class TestArticle extends AndroidTestCase {
         assertEquals(Article.getAll().size(), Article.count());
         article.delete();
     }
+
+    public void testShouldGetArticleOnDataBase() throws ClassNotFoundException, SQLException {
+        Article articleOne = new Article();
+
+        articleOne.setPublishedJournals(Integer.parseInt("0"));
+        articleOne.setPublishedConferenceProceedings(Integer.parseInt("0"));
+        articleOne.save();
+
+        Article articleTwo = Article.get(Article.last().getId());
+
+        assertEquals(articleOne.getPublishedJournals(), articleTwo.getPublishedJournals());
+        assertEquals(articleOne.getPublishedConferenceProceedings(),
+                articleTwo.getPublishedConferenceProceedings());
+
+        articleOne.delete();
+        articleTwo.delete();
+    }
+
+    public void testShouldGetAllArticleOnDataBase() throws ClassNotFoundException, SQLException {
+        int totalArticles = Article.count();
+
+        assertEquals(totalArticles, Article.getAll().size());
+        assertEquals("", Article.first().get("test"));
+    }
+
+    public void testShouldGetTheFirstArticleOnDataBase() throws ClassNotFoundException, SQLException {
+        Article firstArticle = Article.first();
+
+        assertEquals(firstArticle.getPublishedJournals(),
+                Article.getAll().get(0).getPublishedJournals());
+        assertEquals(firstArticle.getPublishedConferenceProceedings(),
+                Article.getAll().get(0).getPublishedConferenceProceedings());
+    }
+
+    public void testShouldGetTheLastArticleOnDataBase() throws ClassNotFoundException, SQLException {
+        Article lastArticle = Article.last();
+        ArrayList<Article> articleList = Article.getAll();
+
+        assertEquals(lastArticle.getPublishedJournals(),
+                articleList.get(articleList.size()-1).getPublishedJournals());
+        assertEquals(lastArticle.getPublishedConferenceProceedings(),
+                articleList.get(articleList.size()-1).getPublishedConferenceProceedings());
+    }
+
+    
 }

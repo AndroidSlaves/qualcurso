063c2d5a0094996ec5069f22b7e32bf0e21e8966
Pedro Ivo <andradepedroivo@gmail.com>
2016-06-20 03:33:53 -0300

    Adding TestArticle database tests

--- QualCurso/app/src/androidTest/java/model/TestArticle.java
+++ QualCurso/app/src/androidTest/java/model/TestArticle.java
@@ -1,5 +1,6 @@
 package model;
 
+import android.database.SQLException;
 import android.test.AndroidTestCase;
 
 import libraries.DataBaseStructures;
@@ -31,5 +32,28 @@ public class TestArticle extends AndroidTestCase {
         super.tearDown();
     }
 
-    
+    public void testShouldCreateNewArticleOnDataBase() throws ClassNotFoundException, SQLException {
+        int initialCount = Article.count();
+
+        Article article = new Article();
+        article.setPublishedJournals(Integer.parseInt("0"));
+        article.setPublishedConferenceProceedings(Integer.parseInt("0"));
+
+        assertTrue(article.save());
+        assertEquals(initialCount, Article.count() - 1);
+        article.delete();
+    }
+
+    public void testShouldCountArticleOnDataBase() throws ClassNotFoundException, SQLException {
+        int initialCount = Article.count();
+        Article article = new Article();
+
+        article.setPublishedJournals(Integer.parseInt("0"));
+        article.setPublishedConferenceProceedings(Integer.parseInt("0"));
+        article.save();
+
+        assertEquals(initialCount+1, Article.count());
+        assertEquals(Article.getAll().size(), Article.count());
+        article.delete();
+    }
 }
